5afceb298d0302f4d5da78c453fada021d1fd85d
David Santamaria Lago <dsantamarialago@stubhub.com>
2018-12-09 13:24:24 +0100

    fixed data initialization

--- storage/src/main/resources/data-h2.sql
+++ storage/src/main/resources/data-h2.sql
@@ -1,18 +1,16 @@
-/*insert into centro_deportivo (id, nombre) values (1, 'FADURA');
-insert into centro_deportivo (id, nombre) values (2, 'GOBELA');
-insert into centro_deportivo (id, nombre) values (3, 'ANDRA-MARI');
-insert into centro_deportivo (id, nombre) values (4, 'ROMO');
+insert into centro_deportivo (id, nombre, created_at, updated_at) values (1, 'FADURA', now(), now());
+insert into centro_deportivo (id, nombre, created_at, updated_at) values (2, 'GOBELA', now(), now());
+insert into centro_deportivo (id, nombre, created_at, updated_at) values (3, 'ANDRA-MARI', now(), now());
+insert into centro_deportivo (id, nombre, created_at, updated_at) values (4, 'ROMO', now(), now());
 
 
-insert into centro_salud (id, nombre) values (1, 'CRUCES');
-insert into centro_salud (id, nombre) values (2, 'AMBULATORIO-ALGORTA');
-insert into centro_salud (id, nombre) values (3, 'AMBULATORIO-GETXO');
-insert into centro_salud (id, nombre) values (4, 'BASURTO');
+insert into centro_salud (id, nombre, created_at, updated_at) values (1, 'CRUCES', now(), now());
+insert into centro_salud (id, nombre, created_at, updated_at) values (2, 'AMBULATORIO-ALGORTA', now(), now());
+insert into centro_salud (id, nombre, created_at, updated_at) values (3, 'AMBULATORIO-GETXO', now(), now());
+insert into centro_salud (id, nombre, created_at, updated_at) values (4, 'BASURTO', now(), now());
 
 
-insert into medico (id, nss, nombre, apellidos, centro_salud) values (1, 123451, 'nombre1', 'apellido11 apellido12',1);
-insert into medico (id, nss, nombre, apellidos, centro_salud) values (2, 123452, 'nombre1', 'apellido11 apellido12',2);
-insert into medico (id, nss, nombre, apellidos, centro_salud) values (3, 123453, 'nombre1', 'apellido11 apellido12',1);
-insert into medico (id, nss, nombre, apellidos, centro_salud) values (4, 123454, 'nombre1', 'apellido11 apellido12',4);
-
-*/
+insert into medico (id, nss, nombre, apellidos, centro_salud_id, created_at, updated_at) values (1, 123451, 'nombre1', 'apellido11 apellido12',1, now(), now());
+insert into medico (id, nss, nombre, apellidos, centro_salud_id, created_at, updated_at) values (2, 123452, 'nombre1', 'apellido11 apellido12',2, now(), now());
+insert into medico (id, nss, nombre, apellidos, centro_salud_id, created_at, updated_at) values (3, 123453, 'nombre1', 'apellido11 apellido12',1, now(), now());
+insert into medico (id, nss, nombre, apellidos, centro_salud_id, created_at, updated_at) values (4, 123454, 'nombre1', 'apellido11 apellido12',4, now(), now());
