c9e9417c812837055c44f27a8bc8a8de8de79f2d
David Santamaria Lago <dsantamarialago@stubhub.com>
2018-12-12 13:04:43 +0100

    Fixed main test

--- storage/src/test/java/com/qualit/kirolrz/storage/StorageApplicationTests.java
+++ storage/src/test/java/com/qualit/kirolrz/storage/StorageApplicationTests.java
@@ -1,11 +1,19 @@
 package com.qualit.kirolrz.storage;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.assertj.ApplicationContextAssert;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StorageApplicationTests {
+
+    @Test
+    public void StorageApplicationContexLoadsTest(){
+
+    }
 
 }
